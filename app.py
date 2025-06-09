c0bcd852001a267949d770d3e14c8adbdc62ec06
Achintya <57226456+Achintya-Mandal@users.noreply.github.com>
2025-06-09 21:54:26 +0530

    hi

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import webbrowser
 
 st.title("PMD Functional Chatbot")
 
@@ -12,27 +13,29 @@ with st.form(key='chat_form'):
     submit_button = st.form_submit_button(label='Send')
 
 if submit_button and input_string:
-    backend_api = "https://synonyms_service-sleepy-warthog-hg.cfapps.eu12.hana.ondemand.com/synonym_query?input_string=" + input_string
-    response = requests.get(
-        backend_api,
-        #params={"message": input_string},  # Use 'params' for GET requests
-        verify=False  # Disable SSL verification
-    )
-    if response.status_code == 200:
-        try:
-            # Handle plain text response
-            response_message = response.text.strip()  # Use response.text instead of response.json()
-            if response_message:
-                # Append user input and response to chat history
-                st.session_state.chat_history.append({"user": input_string, "bot": response_message})
-            else:
-                st.write("Error: Empty response from the server.")
-        except Exception as e:
-            st.write("Error: Unable to process the response.")
-            st.write(f"Exception: {e}")
-    else:
-        st.write(f"Error: {response.status_code} - {response.reason}")
-        st.write("Response content:", response.text)
+    url = "https://www.linkedin.com"
+    webbrowser.open(url)
+#     backend_api = "https://synonyms_service-sleepy-warthog-hg.cfapps.eu12.hana.ondemand.com/synonym_query?input_string=" + input_string
+#     response = requests.get(
+#         backend_api,
+#         #params={"message": input_string},  # Use 'params' for GET requests
+#         verify=False  # Disable SSL verification
+#     )
+#     if response.status_code == 200:
+#         try:
+#             # Handle plain text response
+#             response_message = response.text.strip()  # Use response.text instead of response.json()
+#             if response_message:
+#                 # Append user input and response to chat history
+#                 st.session_state.chat_history.append({"user": input_string, "bot": response_message})
+#             else:
+#                 st.write("Error: Empty response from the server.")
+#         except Exception as e:
+#             st.write("Error: Unable to process the response.")
+#             st.write(f"Exception: {e}")
+#     else:
+#         st.write(f"Error: {response.status_code} - {response.reason}")
+#         st.write("Response content:", response.text)
 
 # Display chat history
 if st.session_state.chat_history:
